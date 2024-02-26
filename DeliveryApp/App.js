5967d4a70e74f144e391e7fa0c5e04ad0e29a4a1
studentocsbMatvei <matvei.plavskii@stu.ocsb.ca>
2024-02-26 09:11:12 -0500

    react-navigation package is added

--- DeliveryApp/App.js
+++ DeliveryApp/App.js
@@ -1,5 +1,11 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
