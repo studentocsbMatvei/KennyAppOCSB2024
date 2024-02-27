ccff04171621ef89396461cbbd6c6cafa94f96fa
studentocsbMatvei <matvei.plavskii@stu.ocsb.ca>
2024-02-27 08:54:03 -0500

    added navigation component

--- DeliveryApp/App.js
+++ DeliveryApp/App.js
@@ -3,18 +3,39 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+function KennysPage({navigation}) {
+  return (
+    <View>
+      <Text>
+        That's a page for Kenny!
+      </Text>
+    </View>
+  )
+}
+
+function HomeScreen({navigation}) {
   return (
     <View style={styles.container}>
       <Text style={styles.textBackground}>Choose if you are Kenny or teacher</Text>
-      <Button title="I am Kenny"/>
+      <Button title="I am Kenny" onPress={() => navigation.navigate('KennysPage')}/>
       <Button title="I am a teacher"/>
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="KennysPage" component={KennysPage}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
