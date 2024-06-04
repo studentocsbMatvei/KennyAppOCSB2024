ddd97401760c73274cb2f85024e89f3d809767af
studentocsbMatvei <matvei.plavskii@stu.ocsb.ca>
2024-06-04 08:33:05 -0400

    "accept_data" api endpoint is simplified

--- DeliveryApp/server_side/server.js
+++ DeliveryApp/server_side/server.js
@@ -10,8 +10,6 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
-let storedData = "";
-
 app.listen(PORT, () => {
     console.log('Server is running on port: ' + PORT);
 });
@@ -25,7 +23,6 @@ app.get('/api/data', (req, res) => {
 
 app.post('/api/accept_data', (req, res) => {
     const recievedData = req.body.data;
-    storedData = recievedData;
-    console.log('Recieved and stored data: ' + storedData)
+    console.log('Recieved and stored data: ' + recievedData)
     res.send('Data stored successfully!');
 });
