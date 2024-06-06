c8dd1468ac5626c9ca9056bb78a9d5788b397122
studentocsbMatvei <matvei.plavskii@stu.ocsb.ca>
2024-06-06 09:37:47 -0400

    added all important api endpoints into the server.js file

--- DeliveryApp/server_side/server.js
+++ DeliveryApp/server_side/server.js
@@ -23,6 +23,36 @@ app.get('/api/data', (req, res) => {
 
 app.post('/api/accept_data', (req, res) => {
     const recievedData = req.body.data;
-    console.log('Recieved and stored data: ' + recievedData)
+    console.log('Recieved and stored data: ' + recievedData);
     res.send('Data stored successfully!');
+});
+
+app.post('/api/accept_room_number', (req, res) => {
+    const recievedNumber = req.body.data;
+    console.log('Recieved and stored the room number: ' + recievedNumber);
+    res.send('room number stored successfully!');
+});
+
+app.post('/api/accept_name', (req, res) => {
+    const recievedName = req.body.data;
+    console.log('Recieved and stored name: ' + recievedName);
+    res.send('name is stored successfully!');
+});
+
+app.post('/api/accept_email', (req, res) => {
+    const recievedEmail = req.body.data;
+    console.log('Recived and stored email: ' + recievedEmail);
+    res.send('email is stored successfully!');
+});
+
+app.post('api/accept_time', (req, res) => {
+    const recievedTime = req.body.data;
+    console.log('Recieved and stored delivery time: ' + recievedTime);
+    res.send('time is stored successfully!');
+});
+
+app.post('api/accept_comments', (req, res) => {
+    const recievedComment = req.body.data;
+    console.log('Recieved and stored comment: ' + recievedComment);
+    res.send('comment is stored successfully!');
 });
